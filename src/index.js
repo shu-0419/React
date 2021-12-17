0734c6964f5ea4abf8acce717fcdb535d828a422
shu-0419 <91708596+shu-0419@users.noreply.github.com>
2021-12-17 09:46:48 +0900

    filter、map

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
-const val = "const変数";
-console.log(val);
+// // const val = "const変数";
+// console.log(val);
+
+/**
+ * スプレッド構文
+ */
+//配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+//まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...num3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+// const nameArr = ["田中", "山田", "石川"];
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// })
+
+// nameArr.map((name) => console.log(name));
+
+const numArr = [1, 2, 3, 4, 5];
+for(let index = 0; index < numArr.)
+
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// })
+// console.log(newNumArr);
